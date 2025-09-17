09e3f82a3f3c9ae24588c5358b1d072d75754347
ihassanarif29 <ihassanarif29@gmail.com>
2025-09-18 03:09:34 +0500

    Add Fragments

--- app/src/main/java/com/example/androidkotlinnavigationdrawerexampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinnavigationdrawerexampleapplication/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.androidkotlinnavigationdrawerexampleapplication
 
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var toolbar: Toolbar
-    private lateinit var mainText: TextView
+    private lateinit var container: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.navigation_view)
         toolbar = findViewById(R.id.toolbar)
-        mainText = findViewById(R.id.main_text)
+        container = findViewById(R.id.fragment_container)
 
         // Setup Toolbar
         setSupportActionBar(toolbar)
@@ -35,12 +36,19 @@ class MainActivity : AppCompatActivity() {
         // Handle item clicks
         navigationView.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.nav_home -> mainText.text = "Home Selected"
-                R.id.nav_profile -> mainText.text = "Profile Selected"
-                R.id.nav_settings -> mainText.text = "Settings Selected"
+                R.id.nav_home -> replaceFragment( HomeFragment())
+                R.id.nav_profile -> replaceFragment(ProfileFragment())
+                R.id.nav_settings -> replaceFragment(SettingsFragment())
             }
             drawerLayout.closeDrawers()
             true
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
